135edf56a8972f714c80b8e1dd9b03c2ed3a57f7
Elad Maor <EladMaor17.8@gmail.com>
2016-02-03 00:18:01 +0200

    Add Marker tool, And improved Quiz

---
+++ Reader - HTML/js/markerTool.js
@@ -0,0 +1,94 @@
+$(document).ready(function() {
+    var bIsMarkerActive = false;
+    var selectedColor = "remove";
+    
+    $("#marker-tool").click(function() {
+        bIsMarkerActive = true;
+    })
+    
+    $("#cursor-tool").click(function() {
+        bIsMarkerActive = false;
+    })
+    
+    $("#flipbook").mouseup(function() {
+        if (elementContainsSelection(document.getElementById('flipbook')) && bIsMarkerActive) {
+            highlightSelection('yellow');
+        }
+    })
+});
+
+// the function is hilighting the text with the selected color
+function makeEditableAndHighlight(colour) {
+    var range, sel = window.getSelection();
+    if (sel.rangeCount && sel.getRangeAt) {
+        range = sel.getRangeAt(0);
+    }
+    document.designMode = "on";
+    if (range) {
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+    // Use HiliteColor since some browsers apply BackColor to the whole block
+    if (!document.execCommand("HiliteColor", false, colour)) {
+        document.execCommand("BackColor", false, colour);
+    }
+    document.designMode = "off";
+    
+    // unselect the selection
+    if (window.getSelection) {
+      if (window.getSelection().empty) {  // Chrome
+        window.getSelection().empty();
+      } else if (window.getSelection().removeAllRanges) {  // Firefox
+        window.getSelection().removeAllRanges();
+      }
+    } else if (document.selection) {  // IE?
+      document.selection.empty();
+    }
+}
+
+function highlightSelection(colour) {
+    var range, sel;
+    if (window.getSelection) {
+        // IE9 and non-IE
+        try {
+            if (!document.execCommand("BackColor", false, colour)) {
+                makeEditableAndHighlight(colour);
+            }
+        } catch (ex) {
+            makeEditableAndHighlight(colour)
+        }
+    } else if (document.selection && document.selection.createRange) {
+        // IE <= 8 case
+        range = document.selection.createRange();
+        range.execCommand("BackColor", false, colour);
+    }
+}
+
+// checks if the selection is inside the recived container id
+function isOrContains(node, container) {
+    while (node) {
+        if (node === container) {
+            return true;
+        }
+        node = node.parentNode;
+    }
+    return false;
+}
+
+function elementContainsSelection(el) {
+    var sel;
+    if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount > 0) {
+            for (var i = 0; i < sel.rangeCount; ++i) {
+                if (!isOrContains(sel.getRangeAt(i).commonAncestorContainer, el)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    } else if ( (sel = document.selection) && sel.type != "Control") {
+        return isOrContains(sel.createRange().parentElement(), el);
+    }
+    return false;
+}
