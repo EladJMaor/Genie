aff6d9d7eb1a4563efe31badf8b104195e095259
gidi <rgidi@cambium.co.il>
2016-02-03 13:14:30 +0200

    size of window

--- Reader - HTML/js/book.js
+++ Reader - HTML/js/book.js
@@ -1,7 +1,28 @@
-$(function(){
+$(function () {
+
+    // variable for set height and width of elements
+    var windowWidth;
+    var windowHeight;
+    var widhtRatioOfIframe;
+    var widthRatioOfbook;
+
+    function setSizeVariables() {
+        windowWidth = $(window).width();
+        windowHeight = $(window).height();
+
+        widthRatioOfbook = 0.55;
+
+        if (windowWidth < 1400) {
+            widhtRatioOfIframe = 0.4;
+        }
+        else {
+            widhtRatioOfIframe = 0.55;
+        }
+    }
+    setSizeVariables();
 
     //Define xml-store variable
-    var book_path="./books/sample_book.xml", book_progress = 20, last_page = '1';
+    var book_path = "./books/sample_book.xml", book_progress = 20, last_page = '1';
     // Ajax POST request to load the book
     $.ajax({
         type: "GET",
@@ -18,27 +39,27 @@ $(function(){
 
             //Add the cover title
             toc.append("<li><a>" + xml.find("contents cover title").text() + "</a></li>");
-            toc.children("li").children("a").attr("value" , xml.find("contents cover page").text());
+            toc.children("li").children("a").attr("value", xml.find("contents cover page").text());
 
             //Populate ToC
-            xml.find("contents chapter").each(function(){
+            xml.find("contents chapter").each(function () {
                 //adds the title
                 var title = $(this).children("title").text();
                 toc.append("<li><a>" + title + "</a></li>");
-                
-                var pagelocation = $(this).children("page").text();//saves the page location
-                toc.children("li").children("a")[toc.children("li").children("a").length - 1].setAttribute("value",pagelocation )
-                
-                $(this).find("sub").each(function(){
-                    toc.append("<ul><li><a>" + $(this).children("title").text() +  "</a></li></ul>");
+
+                var pagelocation = $(this).children("page").text(); //saves the page location
+                toc.children("li").children("a")[toc.children("li").children("a").length - 1].setAttribute("value", pagelocation)
+
+                $(this).find("sub").each(function () {
+                    toc.append("<ul><li><a>" + $(this).children("title").text() + "</a></li></ul>");
                     pagelocation = $(this).children("page").text();
-                    toc.children("ul").children("li").children("a")[toc.children("ul").children("li").children("a").length - 1].setAttribute("value",pagelocation ); 
+                    toc.children("ul").children("li").children("a")[toc.children("ul").children("li").children("a").length - 1].setAttribute("value", pagelocation);
                 })
             })
-            
-            $("[value]").click(function(){
+
+            $("[value]").click(function () {
                 pageLocation = this.getAttribute("value");
-                $("#flipbook").turn("page" , pageLocation);
+                $("#flipbook").turn("page", pageLocation);
             })
 
             ///Load cover page
@@ -50,74 +71,89 @@ $(function(){
 
             //Load turn.js book
             $("#flipbook").turn({
-                width: width * 0.55,
-                height: width * 0.55 * 0.71,
+                width: windowWidth * widthRatioOfbook,
+                height: windowWidth * widthRatioOfbook * 0.71,
                 duration: 1500,
                 autoCenter: true
             });
-            
+
             // change the view to a single page
             $("#flipbook").turn('display', 'single');
-            
+
             // change flipbook direction to right-to-left
             $("#flipbook").turn("direction", "rtl");
-          
+
+            // setup iframe height
+            if (width < 1400) {
+                widhtRatioOfIframe = 0.4;
+            }
+            else {
+                widhtRatioOfIframe = 0.55;
+            }
+
+            jQuery.each(document.getElementsByTagName('iframe'), function (i, val) {
+                $(val).height(windowWidth * widthRatioOfbook * 0.71 * widhtRatioOfIframe);
+            });
+
         },
 
-        error: function(){
+        error: function () {
             console.log("Error loading book.");
         }
     });
 
     // resize book while window resize.
     $(window).resize(function () {
-        var width = $(window).width();
-        var height = $(window).height();
+        setSizeVariables();
 
-        $("#flipbook").turn("size", width * 0.55, width * 0.55 * 0.71);
+        $("#flipbook").turn("size", windowWidth * widthRatioOfbook, windowWidth * widthRatioOfbook * 0.71);
+
+        jQuery.each(document.getElementsByTagName('iframe'), function (i, val) {
+            $(val).height(windowWidth * widthRatioOfbook * 0.71 * widhtRatioOfIframe);
+        });
     });
-    
+
     // disable the previous button
     $("div#right").addClass("disabled-btn");
-    
+
     // turn to the previous page
     $("div#right").click(function () {
         $("#flipbook").turn("previous");
     });
-    
+
     // turn to the previous page
     $("div#left").click(function () {
         $("#flipbook").turn("next");
     });
-    
+
     //resturns the buttons when now in the last/first 
-    $("#flipbook").bind("turning", function(event,page,view) {
-        
-        $("video").each(function(){
+    $("#flipbook").bind("turning", function (event, page, view) {
+
+        $("video").each(function () {
         });
-        
-        
-        
+
+
+
         if (page > 1) {
             $("div#right").removeClass("disabled-btn");
         }
-        if(page < $("#flipbook").turn("pages")){
-            
+        if (page < $("#flipbook").turn("pages")) {
+
             $("div#left").removeClass("disabled-btn");
         }
     });
-    
 
-    $("#flipbook").bind("last", function(event) {
+
+    $("#flipbook").bind("last", function (event) {
         $("div#left").addClass("disabled-btn");
     });
-    
-    $("#flipbook").bind("first", function(event) {
-    $("div#right").addClass("disabled-btn");
+
+    $("#flipbook").bind("first", function (event) {
+        $("div#right").addClass("disabled-btn");
     });
-    
-    $("#flipbook").bind("start",function(event, pageObject, corner){
-       console.log(pageObject.turn); 
+
+    $("#flipbook").bind("start", function (event, pageObject, corner) {
+        console.log(pageObject.turn);
     });
 });
 
